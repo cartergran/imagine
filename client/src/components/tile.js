158f28ad9ef3363c0ce6e5a61c1e3199cdbe67bd
Carter Gran <cartergran@gmail.com>
2023-12-26 23:35:18 -0800

    add transition to tile

--- client/src/components/tile.js
+++ client/src/components/tile.js
@@ -1,18 +1,38 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import styled from "styled-components";
 import axios from "axios";
+import { Transition } from "react-transition-group";
 
 const StyledTile = styled.div`
   width: 100px;
   height: 100px;
 
   border: 1px solid white;
+`;
+
+const duration = 800;
+
+const transitionStyles = {
+  entering: 1,
+  entered: 1,
+  exiting: 0,
+  exited: 0,
+};
+
+const StyledImage = styled.div`
+  width: 100%;
+  height: 100%;
+
   background: url('data:image/png;base64,${props => props.$img || ''}');
   background-size: contain;
+
+  transition: opacity ${props => props.$duration || 300}ms ease-in-out 0s;
+  opacity: ${props => props.$opacity || 0};
 `;
 
 export default function Tile({ loc }) {
   const [img, setImg] = useState("");
+  const nodeRef = useRef(null);
 
   let getImg = async (r, c) => {
     try {
@@ -24,13 +44,22 @@ export default function Tile({ loc }) {
   };
 
   let handleClick = (e) => {
-    let [r, c] = e.target.children[0].getAttribute("loc").split("");
+    let [r, c] = loc;
     getImg(r, c);
   };
 
   return (
-    <StyledTile onClick={handleClick} $img={img}>
-      <div loc={loc}></div>
+    <StyledTile onClick={handleClick}>
+      <Transition nodeRef={nodeRef} in={img !== ""} timeout={duration}>
+        {state => (
+          <StyledImage
+            ref={nodeRef}
+            $img={img}
+            $duration={duration}
+            $opacity={transitionStyles[state]}
+          />
+        )}
+      </Transition>
     </StyledTile>
   );
 }
