83642536c494b5888888d41fd4e192e815a19d3f
Carter Gran <cartergran@gmail.com>
2025-12-02 23:39:21 -0500

    fix: wdyr

--- client/src/utils/wdyr.js
+++ client/src/utils/wdyr.js
@@ -1,12 +1,11 @@
-// import React from 'react';
+import React from 'react';
 
-// if (import.meta.env.VITE_NODE_ENV === 'dev') {
-//   import('@welldone-software/why-did-you-render')
-//     .then((whyDidYouRender) => {
-//       whyDidYouRender.default(React, {
-//         trackAllPureComponents: true,
-//         logOnDifferentValues: true
-//       });
-//     })
-//     .catch(() => { /* silently ignore */ });
-// }
+if (import.meta.env.VITE_NODE_ENV === 'dev' && import.meta.env.VITE_WHY_DID_YOU_RENDER === 'true') {
+  const { default: wdyr } = await import('@welldone-software/why-did-you-render');
+
+  wdyr(React, {
+    include: [/.*/],
+    trackAllPureComponents: true,
+    trackHooks: true,
+  });
+}
